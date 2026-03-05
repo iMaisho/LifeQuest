0dcf9638b4ff8636fe81f1dd8f29395610c9d037
A. MINGAM <antonin.mingam@gmail.com>
2026-03-05 16:13:42 +0100

    feat: authentification avec scopes

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :lifequest, :scopes,
+  user: [
+    default: true,
+    module: Lifequest.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :binary_id,
+    schema_table: :users,
+    test_data_fixture: Lifequest.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :lifequest,
   ecto_repos: [Lifequest.Repo],
   generators: [timestamp_type: :utc_datetime, binary_id: true]
